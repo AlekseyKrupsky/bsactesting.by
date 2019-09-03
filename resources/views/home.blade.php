6997eaf66e93cf7c7cded64e21fcef825bdb6896
LordAlexey <dachadybenci2@gmail.com>
2019-09-03 10:21:40 +0300

    additional information on main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,27 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Добро пожаловать на <b>Bsac Testing!</b></div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <p>
+                        <b>Bsac Testing</b> - веб-приложение, которое позволяет проводить тестирование знаний обучающихся.
+                        Данное приложение разработано студентами Белорусской государственной академии связи. Здесь присутствует возвожность
+                        создания тестов любой сложности, а так же функционал для их автоматической проверки.
+                    </p>
+                    <p>
+                        Ссылка на сайт академии <a href="http://bsac.by/">bsac.by</a>
+                    </p>
+                    <p>Вы так же можете ознакомится с другими проектами студентов Академии:</p>
+                    <ul>
+                        <li>
+                        <a href="http://bsac.by/projects/conference/">Конференция</a>
+                        </li>
+                        <li><a href="http://bsac.by/projects/youth-in-science/">Молодежь в науке</a></li>
+                        <li><a href="http://bsac.by/projects/learning-center/">Учебный центр</a></li>
+                        <li><a href="http://umc.bsac.by/">Почтовая безопасность</a></li>
+                        <li><a href="http://rfsc.vsol.org/">Калькулятор платы за спектр</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
